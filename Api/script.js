3183c5e4a10d86131ae824816b44c3379ddc75af
lolasegui <lsegui19939@iesjoanramis.org>
2025-01-16 08:57:23 +0100

    Add files via upload

--- Api/script.js
+++ Api/script.js
@@ -1,6 +1,6 @@
 document.getElementById("button").addEventListener("click", function () {
     const xhr = new XMLHttpRequest();
-
+    document.getElementById("resultat").innerHTML = "";
     xhr.addEventListener('readystatechange', function () {
         if (this.readyState === this.DONE) {
             const response = JSON.parse(this.responseText);
@@ -21,6 +21,7 @@ document.getElementById("tots").addEventListener("click", function () {
         if (this.readyState === this.DONE) {
             const response = JSON.parse(this.responseText);
             console.log(response);
+            document.getElementById("resultat").innerHTML = "";
             for (let i = 0; i < response.results.length; i++) {
 
                 const xhr1 = new XMLHttpRequest();
@@ -28,7 +29,7 @@ document.getElementById("tots").addEventListener("click", function () {
                 xhr1.addEventListener('readystatechange', function () {
                     if (this.readyState === this.DONE) {
                         const response = JSON.parse(this.responseText);
-                        pintarPokemon(response);
+                        pintarTots(response);
                     }
                 });
 
@@ -61,3 +62,68 @@ function pintarPokemon(pokemon) {
     
 
 }
+
+function pintarTots(pokemon) {
+
+    let nom = document.createElement("h1")
+    let imatge = document.createElement("img")
+   
+    nom.innerHTML = pokemon.name;
+    imatge.src = pokemon.sprites.front_default;
+
+   
+  
+    document.getElementById("resultat").append(nom)
+    document.getElementById("resultat").append(imatge)
+
+
+
+}
+document.getElementById("tipo").addEventListener("click", function () {
+    const xhr = new XMLHttpRequest();
+
+    xhr.addEventListener('readystatechange', function () {
+        if (this.readyState === this.DONE) {
+            const response = JSON.parse(this.responseText);
+            console.log(response);
+            document.getElementById("resultat").innerHTML = "";
+            for (let i = 0; i < response.results.length; i++) {
+            let tipo = document.getElementById("gibet").value
+                const xhr1 = new XMLHttpRequest();
+
+                xhr1.addEventListener('readystatechange', function () {
+                    if (this.readyState === this.DONE) {
+                        const response = JSON.parse(this.responseText);
+                        console.log(response)
+                        if(response.types[0].type.name==tipo){
+                            pintarPokemon2(response);
+                        }
+                        
+                    }
+                });
+
+                xhr1.open('GET', response.results[i].url);
+                xhr1.send();
+
+            }
+        }
+    });
+
+    xhr.open('GET', `https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0`);
+    xhr.send();
+})
+
+
+function pintarPokemon2 (pokemon) {
+
+    let nom = document.createElement("h1")
+    let imatge = document.createElement("img")
+   
+    nom.innerHTML = pokemon.name;
+    imatge.src = pokemon.sprites.front_default;
+  
+    document.getElementById("resultat").append(nom)
+    document.getElementById("resultat").append(imatge)
+}
+
+
